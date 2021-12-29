33eb60765360d2d26f5822164ee1736395ae7dd9
Nguyễn Thanh Vũ <91299357+Tuitenvu@users.noreply.github.com>
2021-12-29 13:43:40 +0000

    Add files via upload

---
+++ thongtinthanhtoan/config.js
@@ -0,0 +1,36 @@
+const WEB = {
+    title: "Thông tin thanh toán",
+    favicon: "logoweb.png",
+};
+
+const USER = {
+    username: "NGUYEN THANH VU",
+    blueTick: false,
+    avatar: "a.jpg",
+    skills: [
+        "FACEBOOK",
+        "DESIGNER"
+    ],
+    banks: [
+        {
+            name: "MB BANK",
+            logo: "mbbank.png",
+            number: "0000155529685",
+            owner: "NGUYEN THANH VU"
+        }, 
+        {
+            name: "MOMO",
+            logo: "momo.png",
+            number: "0866810260",
+            owner: "NGUYEN THANH VU",
+            fastTransfer: true
+        }, 
+        {
+            name: "THESIEURE",
+            logo: "tsr.png",
+            number: "0866810260",
+            owner: "Nguyễn Thanh Vũ"
+        },
+
+    ]
+};
